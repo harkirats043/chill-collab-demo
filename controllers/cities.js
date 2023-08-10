7e95a67e3597567e393296061ab7101f51bc1fed
Alex Stepanov <stepanov.hw@gmail.com>
2023-08-10 13:47:08 -0600

    Change header

--- controllers/cities.js
+++ controllers/cities.js
@@ -17,13 +17,40 @@ const provinces= [
 ]
 module.exports = {
     new: newCity,
-    create: createOne
+    create: createOne,
+    index
 }
 
 function newCity (req,res) {
     res.render('cities/new', {errorMsg: "", title: "Add Canadian City",provinces})
 }
 
-function createOne(req,res){
+async function createOne(req,res){
+    // res.send(req.body)
+    const cityData = { ...req.body };
+    if(cityData.population) {
+        cityData.population = parseInt(cityData.population)
+    }
+
+    try {
+        const createdCity = await City.create(cityData);
+        console.log(createdCity);
+         res.redirect(`/cities`);
+      } catch (err) {
+        console.log(err);
+        res.render("cities/new", { errorMsg: err.message });
+      }
 
 }
+
+
+
+async function index(req, res) {
+    try {
+      const results = await City.find({}).sort('city');
+      res.render("cities/index", { cities: results, title: "All Cities" });
+    } catch (err) {
+      console.log(err);
+      res.render("cities/index", { errorMsg: err.message });
+    }
+  }
--- routes/cities.js
+++ routes/cities.js
@@ -7,4 +7,8 @@ router.get('/new', citiesCtrl.new);
 
 router.post("/",citiesCtrl.create)
 
+
+// GET /cities index view
+router.get('/', citiesCtrl.index);
+
 module.exports = router;
